b31571392c28f1c94d49d77323bc0b0a2b877a64
Pedro Pereira <593033+palex65@users.noreply.github.com>
2025-12-18 14:25:50 +0000

    Aula 18Dez (Start)

---
+++ SlotMac/composeApp/src/jvmMain/kotlin/pt/isel/tds/SlotState.kt
@@ -0,0 +1,11 @@
+package pt.isel.tds
+
+/*
+Defina o tipo imutável SlotState (o modelo) para guardar o estado das 3 imagens
+a apresentar. Cada imagem é representada por um byte com valores de 0 a 9
+(10 imagens diferentes). A criação de instâncias de SlotState
+é realizada através da chamada SlotState.random(), que deve retornar uma instância
+com 3 valores/imagens aleatórias.
+A função extensão isWinner deve retornar true caso todos os estados sejam iguais
+*/
+// TODO: II.1
